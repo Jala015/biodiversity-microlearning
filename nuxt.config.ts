aa285b926cd1390f5968e01b2f1712e3576549c1
Jala015 <felipebouabci@gmail.com>
2025-08-18 13:12:06 -0300

    store que lida com o salvamento de decks

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,13 @@
 import tailwindcss from "@tailwindcss/vite";
+import ViteYaml from "@modyfi/vite-plugin-yaml";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), ViteYaml()],
   },
   css: ["~/assets/app.css"],
+  modules: ['@pinia/nuxt', 'pinia-plugin-persistedstate/nuxt'],
 })
